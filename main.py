33aa11537907a179d11eeba96bf3518e925b7246
JovanC7 <Jovanjob7@gmail.com>
2026-04-10 10:53:53 +0200

    * Finished main python and data.json

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-# test
+import json
+
+# r - read
+with open("data.json", 'r') as file:
+    data = json.load(file)
+    data.append({
+        "name" : "Mitar Miric",
+        "age" : 70,
+        "heigh" : 165,
+        "genter" : "male"
+    })
+
+print(data)
+
+# w - write
+# dump - dumpuj - upisi podatke
+# ident = 4 -> lepsi format
+with open ("data.json", 'w') as file:
+    json.dump(data, file, indent = 4)
